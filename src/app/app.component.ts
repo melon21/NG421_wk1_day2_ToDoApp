265ed3f8ec1501c95f77f6070086f73149eed8a6
Melinda Marino <melindamarino22@gmail.com>
2020-04-09 21:20:33 -0500

    working delete button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'NG421wk1day1ToDoApp';
+  title = 'Todos';
+  todoList: any [] = [];
+
+  ngOnInit() {
+    this.todoList = [
+      // example of how to make an item in todo list
+      { title: 'Install Angular CLI', isDone: false },
+    
+    ];
+  }
+  // a method to delete an item
+  deleteTodo(todo:any) {
+    const index = this.todoList.findIndex(todoItem => todoItem === todo);
+    this.todoList.splice(index, 1);
+  }
 }
+
